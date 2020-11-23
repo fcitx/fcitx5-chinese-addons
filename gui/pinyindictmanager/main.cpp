0b1f91a3789d14ceb2ea20a30c7d42490682f868
Weng Xuetian <wengxt@gmail.com>
2020-11-22 17:31:45 -0800

    set locale dir for qt.

--- gui/pinyindictmanager/main.cpp
+++ gui/pinyindictmanager/main.cpp
@@ -7,11 +7,14 @@
 
 #include "main.h"
 #include "pinyindictmanager.h"
+#include <fcitx-utils/i18n.h>
 
 namespace fcitx {
 
 PinyinDictManagerPlugin::PinyinDictManagerPlugin(QObject *parent)
-    : FcitxQtConfigUIPlugin(parent) {}
+    : FcitxQtConfigUIPlugin(parent) {
+    registerDomain("fcitx5-chinese-addons", FCITX_INSTALL_LOCALEDIR);
+}
 
 FcitxQtConfigUIWidget *PinyinDictManagerPlugin::create(const QString &key) {
     Q_UNUSED(key);
